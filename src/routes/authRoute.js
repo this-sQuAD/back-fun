bfb22eb1d52876ca69ac2244ebd07b46ac4ad4a1
lennon.rodrigues <lennon.rodrigues@tallos.com.br>
2023-03-17 17:42:28 -0300

    feature(permissions): Retirado register da controller de Auth

--- src/routes/authRoute.js
+++ src/routes/authRoute.js
@@ -4,7 +4,6 @@ import AuthController from "../controllers/AuthController.js";
 const router = express.Router();
 
 router
-  .post('/auth/register', AuthController.register)
   .post('/auth/login', AuthController.login)
   .post('/auth/forgot_password', AuthController.forgot_password)
   .post('/auth/reset_password', AuthController.reset_password)
